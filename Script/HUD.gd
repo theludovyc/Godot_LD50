30265f57cfada1ac011674c48f07b46067cc92f4
theludovyc <theludovyc@gmail.com>
2022-04-03 00:38:13 +0200

    Update

--- Script/HUD.gd
+++ Script/HUD.gd
@@ -8,14 +8,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func update_dring_arrow(camera_to_player:Vector2, dir_dring:Vector2, dring_distance:float):
-#	if dring_distance > 120:
-#		if !dring_arrow.visible:
-#			dring_arrow.visible = true
-#
-#		dring_arrow.rect_position = get_viewport().size/2 - dring_arrow.rect_size/2 + dir_dring * 120
-#	elif dring_arrow.visible:
-#		dring_arrow.visible = false
-	
-	prints(name, camera_to_player)
-	
-	dring_arrow.rect_position = get_viewport().size/2 + camera_to_player
+	if dring_distance > 120:
+		if !dring_arrow.visible:
+			dring_arrow.visible = true
+
+		dring_arrow.rect_position = get_viewport().size/2 - dring_arrow.rect_size/2 + dir_dring * 120
+	elif dring_arrow.visible:
+		dring_arrow.visible = false
