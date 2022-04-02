554e26af11a1c1e5b455032c5e3d906b0359d052
theludovyc <theludovyc@gmail.com>
2022-04-02 14:55:28 +0200

    Player, Tileset

---
+++ Script/Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const Max_Speed = 300
+const Move_Speed = Max_Speed * 0.5
+const Stop_Speed = Max_Speed * 0.3
+const Gravity = 30
+const JUMP = Gravity * 20
+
+var dir_x:int
+var vel:Vector2
+
+var want_jump := false
+
+onready var label = $Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	want_jump = Input.is_action_just_pressed("ui_up")
+		
+	dir_x = Input.get_axis("ui_left", "ui_right")
+
+func get_speed_x() -> float:
+	if dir_x != 0:
+		return clamp(vel.x+dir_x*Move_Speed, -Max_Speed, Max_Speed)
+	return sign(vel.x)*clamp(abs(vel.x)-Stop_Speed, 0, Max_Speed)
+
+func _physics_process(delta: float) -> void:
+	vel.x = get_speed_x()
+	
+	if is_on_floor():
+		vel.y = Gravity
+		
+		if want_jump:
+			vel.y -= JUMP
+	else:
+		vel.y += Gravity
+	
+	label.text = str(vel)
+	
+	vel = move_and_slide(vel, Vector2.UP)
