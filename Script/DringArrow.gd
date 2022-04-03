4a178d3187693c9c8e7a4587a21047edcfc84ed0
theludovyc <theludovyc@gmail.com>
2022-04-03 17:39:30 +0200

    Fleche

--- Script/DringArrow.gd
+++ Script/DringArrow.gd
@@ -1,15 +1,14 @@
-extends TextureRect
+extends Sprite
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	rect_position -= rect_size/2
-	pass # Replace with function body.
+var my_size = get_rect().size
 
 func do_update(dir_dring:Vector2, dring_distance:float):
 	if dring_distance > 120:
 		if !visible:
 			visible = true
 
-		rect_position = -rect_size/2 + dir_dring * 120
+		position = dir_dring * 120
+		
+		rotation = -dir_dring.angle_to(Vector2.UP)
 	elif visible:
 		visible = false
